986b24adb8982776c3951420026a9464672358f0
hongyinull <80037820+hongyinull@users.noreply.github.com>
2024-10-30 01:28:36 +0800

    Initial Commit

---
+++ moedict2/moedict2App.swift
@@ -0,0 +1,17 @@
+//
+//  moedict2App.swift
+//  moedict2
+//
+//  Created by HONGYINULL on 2024/10/30.
+//
+
+import SwiftUI
+
+@main
+struct moedict2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ moedict2Tests/moedict2Tests.swift
@@ -0,0 +1,16 @@
+//
+//  moedict2Tests.swift
+//  moedict2Tests
+//
+//  Created by HONGYINULL on 2024/10/30.
+//
+
+import Testing
+
+struct moedict2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
